7778619e9860089fc9aba45f89db51c3f5c3799e
Phạm Hoàng Long <108390166+GreenTofu@users.noreply.github.com>
2025-04-09 09:51:07 +0700

    javaBasic1

---
+++ basicJava1/src/SinhVien.java
@@ -0,0 +1,26 @@
+public class SinhVien {
+        String ten;
+
+        static String truong = "HCMUS";
+
+        public void hienThi() {
+            String lop = "20bit";
+
+            System.out.println("Ten: " + ten);
+            System.out.println("Truong: " + truong);
+            System.out.println("Lop: " + lop);
+            System.out.println("Dia chi: " + ThongTin.diachi);
+        }
+
+        public static void main(String[] args) {
+            SinhVien sv = new SinhVien();
+            sv.ten = "Nguyen Van A";
+            sv.hienThi();
+        }
+
+
+    class ThongTin {
+        static String diachi = "Nguyen Van Cu, Q5, HCM";
+    }
+
+}
