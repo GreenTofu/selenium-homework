5f480433dfb8b522304b84c885a8b9e5b9950906
Phạm Hoàng Long <108390166+GreenTofu@users.noreply.github.com>
2025-04-21 11:19:16 +0700

    java oop bai 1

--- javaOOP/src/Bai1/NhanVien.java
+++ javaOOP/src/Bai1/NhanVien.java
@@ -1,4 +1,22 @@
 package Bai1;
 
 public class NhanVien {
+    private String name;
+    private int age;
+    private String address;
+    private String phoneNumber;
+
+    public NhanVien(String name, int age, String address, String phoneNumber) {
+        this.name = name;
+        this.age = age;
+        this.address = address;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public void displayInfo() {
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Address: " + address);
+        System.out.println("Phone Number: " + phoneNumber);
+    }
 }
--- javaOOP/src/Bai1/ThongTin.java
+++ javaOOP/src/Bai1/ThongTin.java
@@ -1,4 +1,14 @@
 package Bai1;
 
 public class ThongTin {
+    public static void main(String[] args) {
+        NhanVien nv1 = new NhanVien(
+                "Nguyen Van A",
+                30,
+                "123 Nguyen Trai, Hanoi",
+                "0123456789"
+        );
+
+        nv1.displayInfo();
+    }
 }
